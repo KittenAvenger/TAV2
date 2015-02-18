82ebc4a166530ed4c1fa879601c08d24b6f59670
David M. Szabo <szabodavidmarcell@gmail.com>
2015-02-18 21:43:26 +0100

    Update TestParser.java
    update it. it still needs the error handling with the not fetched and stuff.

--- src/TestParser.java
+++ src/TestParser.java
@@ -8,7 +8,7 @@ public class TestParser {
 	@Test
 	public void testParseConnection() {
 		
-		String test = "<Accepted connection from  1234 +/>";
+		String test = "<Accepted connection from  \"1234\" +/>";
 		Parser parse = new Parser();
 		String ID = parse.parseRequest(test);
 		
@@ -18,11 +18,33 @@ public class TestParser {
 	@Test
 	public void testParseAdd() {
 		
-		String test = "<AddMessage> <Receiver ID /> <Content TEXT /> </AddMessage>";
+		String test = "<AddMessage> <Receiver \"ID\" /> <Content \"This is my message's.\" /> </AddMessage>";
 		Parser parse = new Parser();
 		String msg = parse.parseAdd(test);
 		
-		assertEquals("TEXT", msg);
+		assertEquals("This is my message's.", msg);
 	}
 	
+	@Test
+	public void testParseDelete() {
+		
+		// need to add if the message doesn't exist or was fetched + 
+		//the msg was not deleted then return an ERROR msg
+		
+		String test = "<DelMessage> <MsgId \"ID\" /> </DelMessage>";
+		Parser parse = new Parser();
+		String msg = parse.parseDelete(test);
+		
+		assertEquals("ID", msg);
+	}
+	
+	@Test
+	public void testParseReplace() {
+		
+		String test = "<RplMessage> <MsgId \"ID\" /> <Content \"TEXT\" /> </RplMessage>";
+		Parser parse = new Parser();
+		String msg = parse.parseReplace(test);
+		
+		assertEquals("TEXT", msg);
+	}
 }
