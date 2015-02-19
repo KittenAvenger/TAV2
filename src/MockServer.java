c81a79e3f1fcbc8ce54beb325049cbf0d6697631
kurdishsamurai <alan.dabagh@gmail.com>
2015-02-19 14:30:18 +0100

    template for mocking after integration

--- src/MockServer.java
+++ src/MockServer.java
@@ -1,17 +1,38 @@
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-
 import org.junit.Test;
 
 
 public class MockServer {
-
+	
 	Server mockServer = mock(Server.class);
+	Client client = new Client();
 	
 	@Test
-	public void serverTest1(){
+	public void serverTestAdd(){
+		
+	
+	}
+	
+	//@Test
+	public void serverTestReplace(){
 		
 	}
-
+	
+	//@Test
+	public void serverTestDelete(){
+	
+		
+	}
+	//@Test
+	public void serverTestFetcg(){
+		
+			
+	}
+	//@Test
+	public void serverTestFetchComplete(){
+		
+			
+	}
 }
