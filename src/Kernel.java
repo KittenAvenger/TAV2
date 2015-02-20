e148631a2e402faff8c6db00878f733ae46e149f
zubayergh <zubayer.ie@gmail.com>
2015-02-20 16:20:47 +0100

    Update Kernel.java

--- src/Kernel.java
+++ src/Kernel.java
@@ -110,8 +110,8 @@ public class Kernel implements WhatsUpGU {
 		for(int i=0; i<server.size(); i++) {
 			if (server.get(i).getReceiver().equals(receiver) && server.get(i).isFetched() == false) {
 				message = message +"\n<Messages>"+ 
-						"\n<Sender '"+server.get(i).getSender()+"' />"+
-						"\n<Content '"+ server.get(i).getMessage()+"' />"+
+						"\n<Sender \""+server.get(i).getSender()+"\" />"+
+						"\n<Content \""+ server.get(i).getMessage()+"\" />"+
 						"\n</Messages>";
 	//System.out.println(message);
 				server.get(i).setFetched();
