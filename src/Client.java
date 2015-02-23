ee754d30f9d752a9a8ebda67e9e4c82827c9c9b6
KittenAvenger <nevadaa51@yahoo.se>
2015-02-23 18:30:52 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -4,110 +4,142 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
 
-public class Client {
+
+
+public class Client 
+{
 	
 	int port = 4444;
-	String str, test;
-	int i = 0;
-	Scanner scan = new Scanner(System.in);
+	String str;		
 	Socket conn = null;
 	
 
-	public String connect() throws IOException{
-		
-		try {
+	public String connect(String ID) throws IOException
+	{
+		try 
+		{
 			conn = new Socket (InetAddress.getLocalHost(), 4444);
-		} catch (UnknownHostException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		} 
+		
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		} 
+		
+		PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
+	    BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));	 
+			 
+	    out.println("<Request connection  " + ID + " +/>" + "\n");
+		
+	    while ((str = in.readLine()) != null && !str.isEmpty()) 
+	    {
+	    	System.out.println("From server: " + str);
+		    break; 
+	    }    
+
+	    return str;   
+	}	
+		
+	public String addMessage(String receiver, String message) throws IOException
+	{		
+		PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
+	    BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
+	    
+	    out.println("<AddMessage> <Receiver \"" + receiver + "\" /> <Content \"" + message + "\" /> </AddMessage>");
+	    
+	    while ((str = in.readLine()) != null && !str.isEmpty()) 
+	    {
+	    	System.out.println("From server: " + str);
+		    break;
 		}
-		
-			 
-			 
-		    PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
-		    BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
-
-		    
-		    out.println("<Request connection  1234567890 +/>" + "\n");
-		
-
-		    while ((str = in.readLine()) != null && !str.isEmpty()) {
-		    	 
-			      System.out.println("From server: " + str);
-			      break;
-			    }
-		    
-		    
-			
-		return str;
-		
-		}	
-		
-	public String addMessage() throws IOException{
+	    
+	    return str;
+	}
+	
+	public String replaceMessage(String msgID, String message) throws IOException
+	{
 		
 		PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
 	    BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
 	    
-	    out.println("<AddMessage> <Receiver \"1234567890\" /> <Content \"This is my message's.\" /> </AddMessage>");
+	    out.println("<RplMessage> <MsgID \"" + msgID + "\" /> <Content \"" + message + "\" /> </RplMessage>");
 	    
-	    while ((str = in.readLine()) != null && !str.isEmpty()) {
-	    	 
-		      System.out.println("From server: " + str);
-		      break;
-		    }
+	    while ((str = in.readLine()) != null && !str.isEmpty()) 
+	    {
+	    	System.out.println("From server: " + str);
+		    break;
+		}
 		
 	    return str;
 	}
+
+
+	public String deleteMessage(String ID) throws IOException
+	{
 	
-	public String DeleteMessage(String ID) throws IOException{
-		
 		PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
 	    BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
 	    
 	    out.println("<DelMessage> <MsgId \"" + ID + "\" /> </DelMessage>");
 	    
-	    while ((str = in.readLine()) != null && !str.isEmpty()) {
-	    	 
-		      System.out.println("From server: " + str);
-		      break;
-		    }
-		
-	    return str;
-	}
+	    while ((str = in.readLine()) != null && !str.isEmpty()) 
+	    {
+	    	System.out.println("From server: " + str);
+		    break;
+		}
 	
-	public String fetchMessage() throws IOException{
-		
+    	return str;
+	}
+
+	public String fetchMessage() throws IOException
+	{
+	
 		PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
 		BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
 	    
-	    	out.println("<FetchMessages/>");
+	    out.println("<FetchMessages/>");
 	    
-	    	while ((str = in.readLine()) != null && !str.isEmpty()) {
-	    	 
-		      System.out.println("From server: " + str);
-		      break;
-		    }
-		
-	    	return str;
+	    while ((str = in.readLine()) != null && !str.isEmpty()) 
+	    {
+	    	System.out.println("From server: " + str);
+		    break;
+		}
+	
+    	return str;
 	}
 
-	public String fetch_complete_Message() throws IOException{
+	public String fetch_complete_Message() throws IOException
+	{
+
+		PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
+		BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
+
+		out.println("<FetchComplete/>");
+
+		while ((str = in.readLine()) != null && !str.isEmpty()) 
+	    {
+	    	System.out.println("From server: " + str);
+		    break;
+		}
+
+		return str;
+	}
+
+	public String disconnect() throws IOException
+	{
 	
 		PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
-    		BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
-    
-    		out.println("<FetchComplete/>");
-    
-    		while ((str = in.readLine()) != null && !str.isEmpty()) {
-    	 
-	      		System.out.println("From server: " + str);
-	      		break;
-		 }
-	
-    		return str;
+	    BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
+	    
+	    out.println("Disconnect");
+	    
+	    while ((str = in.readLine()) != null && !str.isEmpty()) 
+	    {
+	    	System.out.println("From server: " + str);
+		    break;
+		}
+		
+	    return str;
 	}
 }
