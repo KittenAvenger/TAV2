9fcceb641ec2ef68f20b6f2f3279f915d2b0cbec
Dennis Nielsen <nevadaa51@yahoo.se>
2015-02-23 19:43:53 +0100

    clean up

--- src/Parser.java
+++ src/Parser.java
@@ -1,63 +1,49 @@
 
-public class Parser {
-
-	public String parseRequest (String message) {
-		
+public class Parser 
+{
+	public String parseRequest(String message) 
+	{
 		String delims = "[ ]+";
 		String[] tokens = message.split(delims);
 		
 		return tokens[2];
-		
 	}
 	
-	public String parseAdd (String message){
-		
-		String pattern = "[\"]";
-		String[] tokens = message.split(pattern);
-		
-
-		
-		return tokens[3];
-	}
-	
-	public String parseAddID(String message){
-		
-		String pattern = "[\"]";
-		String[] tokens = message.split(pattern);
-		
-		return tokens[1];
-	}
-	
-//	public String msgID(String message){
-//		
-//		String pattern = "[']";
-//		String[] tokens = message.split(pattern);
-//		
-//		return tokens[1];
-//	}
-	
-	// need to add if the message doesn't exist or was fetched + 
-	//the msg was not deleted then return an ERROR msg
-	public String parseDelete (String message){
-		
-		String pattern = "[\"]";
-		String[] tokens = message.split(pattern);
-		
-
-		
-		return tokens[1];
-	}
-	
-	public String parseReplace (String message){
-		
+	public String parseAdd(String message)
+	{
 		String pattern = "[\"]";
 		String[] tokens = message.split(pattern);
 		
 		return tokens[3];
 	}
 	
-	public String parseFetchMessage (String message){
+	public String parseAddID (String message)
+	{
+		String pattern = "[\"]";
+		String[] tokens = message.split(pattern);
 		
+		return tokens[1];
+	}
+	
+	public String parseDelete(String message)
+	{
+		
+		String pattern = "[\"]";
+		String[] tokens = message.split(pattern);
+	
+		return tokens[1];
+	}
+	
+	public String parseReplace(String message)
+	{
+		String pattern = "[\"]";
+		String[] tokens = message.split(pattern);
+		
+		return tokens[3];
+	}
+	
+	public String parseFetchMessage(String message)
+	{
 		String pattern = "[\"]";
 		String[] tokens = message.split(pattern);
 		
@@ -68,6 +54,7 @@ public class Parser {
     {
     	String pattern = "[<>]";
    	 	String[] tokens = msg.split(pattern);
+   	 	
    	 	return tokens[1];
     }
 }
